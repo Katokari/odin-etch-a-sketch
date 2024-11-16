7f57872ba25e0d921081c5f6937a289ec6adb772
Katokari <katokarix@gmail.com>
2024-11-16 14:57:10 +0300

    Add hover effect to the grid

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const container = document.querySelector(".container");
 
+const changeSquareBackground = (e) => {
+    const square = e.target;
+    square.classList.add("hovered");
+}
+
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const square = document.createElement("div");
         square.classList.add("square");
+        square.addEventListener("mouseenter", changeSquareBackground)
         container.appendChild(square);
     }
 }
+
